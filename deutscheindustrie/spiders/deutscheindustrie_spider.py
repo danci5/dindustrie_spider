308164a9b84c9973a50f42a88151e63e9c6cab23
Daniel Homola <homola.daniel@gmail.com>
2019-01-08 22:54:56 +0100

    added more attributes from supplier details

--- deutscheindustrie/spiders/deutscheindustrie_spider.py
+++ deutscheindustrie/spiders/deutscheindustrie_spider.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 
 class QuotesSpider(scrapy.Spider):
     name = "deutscheindustrie"
+    # here you can add more pages with keywords to be crawled, for now just 'Software'
     start_urls = ["https://www.diedeutscheindustrie.de/searches?sterm=Software+&lang=deu&zipcode_city=&suche=ps"]
 
     def __init__(self):
@@ -36,6 +37,12 @@ class QuotesSpider(scrapy.Spider):
         data = {}
         data['website'] = response.css("#firmenprofil > h4.kontakt.fpurls > div > span > a::text").extract_first()
         data['email'] = response.css("#firmenprofil > h4.kontakt.fpmail.openParent > span > a::text").extract_first()
-        # TODO
-        # add other attributes
+        data['address'] = response.css("#firmenprofil > div.komm > div.fpadr > h4:nth-child(1) > span::text").extract_first()
+        data['plz'], data['city'] = response.xpath('//*[@id="firmenprofil"]/div[1]/div[1]/h4[1]/text()').extract()[-1].split()
+        data['categories'] = response.xpath('//ul[@id="fpdatalist"]/li/p/a/text()').extract()
+        data['foundation'] = response.xpath('//*[@class="openParent"][child::h3[text()="Gründungsjahr"]]/p/text()').extract_first()
+        data['employees'] = response.xpath('//*[@class="openParent"][child::h3[text()="Beschäftigte"]]/p/text()').extract_first()
+        data['turnover'] = response.xpath('//*[@class="openParent"][child::h3[text()="Umsatz"]]/p/text()').extract_first()
+        data['telephone'] = response.xpath('//*[@id="firmenprofil"]/div[1]/div[2]/h4[1]/text()').extract_first().strip()
+        data['faxnummer'] = response.xpath('//*[@id="firmenprofil"]/div[1]/div[2]/h4[2]/text()').extract_first().strip()
         yield data
